d1b10b7252e6b03a2f1ace9298217395d78f1dee
Suraj Singla <suraj.singla@mckinleyrice.co>
2021-11-08 10:06:32 +0530

    remove comments

--- src/helper/utils/signTransaction.js
+++ src/helper/utils/signTransaction.js
@@ -1,38 +1,9 @@
-// const performPostRequest = require('./postRequest')
 const { TezosMessageUtils, TezosNodeReader, TezosConstants, TezosNodeWriter } = require('conseiljs')
-
-// const messageCodecEncode = require('./messageCodecEncode') // equivalent to `TezosMessageCodec.encodeOperation`
-
-// function forgeOperations(branch, operations) {
-//     let encoded = TezosMessageUtils.writeBranch(branch);
-//     operations.forEach(m => encoded += messageCodecEncode(m));
-//     return encoded;
-// }
-
-// async function preapplyOperation(server, branch, protocol, operations, signedOpGroup, chainid = 'main') {
-//     const payload = [{
-//         protocol: protocol,
-//         branch: branch,
-//         contents: operations,
-//         signature: signedOpGroup.signature
-//     }];
-
-//     const response = await performPostRequest(server, `chains/${chainid}/blocks/head/helpers/preapply/operations`, payload);
-//     const text = await response.text();
-//     let json;
-//     try {
-//         json = JSON.parse(text);
-//     } catch (err) {
-//         throw new Error(`Could not parse JSON from response of chains/${chainid}/blocks/head/helpers/preapply/operation: '${text}' for ${payload}`);
-//     }
-//     return json;
-// }
 
 async function signOperation(server, operations, signer, offset = TezosConstants.HeadBranchOffset) {
     const blockHead = await TezosNodeReader.getBlockAtOffset(server, offset);
     const blockHash = blockHead.hash.slice(0, 51); 
 
-    // const forgedOperationGroup = forgeOperations(blockHash, operations);
     const forgedOperationGroup = TezosNodeWriter.forgeOperations(blockHash, operations);
 
     const opSignature = await signer.signOperation(Buffer.from(TezosConstants.OperationGroupWatermark + forgedOperationGroup, 'hex'));
